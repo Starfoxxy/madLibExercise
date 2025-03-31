cf900ea1bdc010698fa1e8153a4016d6b50536d5
Starfoxxy <athenajacob97@gmail.com>
2025-03-31 16:19:53 -0400

    Added variables
    Declared and assigned values to variables, including strings, numbers, booleans, and objects with properties;

---
+++ madLibs.js
@@ -0,0 +1,22 @@
+const circusLocation = "hidden circus tent";
+const booleanValue = circusLocation.includes("circus");
+const mathResult = 4 + 5;
+
+let circusAdjective = "crazy";
+let storyTone = "Amusing";
+let clownOutfit = "colorful polka-dot suit";
+let clownPersonality = "wildly funny"
+let clownMessage = "I once tried to juggle spaghetti. It got messy!";
+
+const clown = {
+    name: "Bozo",
+    action: "juggled flaming pineapples",
+    sound: "honk honk",
+}
+
+const clownAction = clown.action;
+
+
+
+
+
